a26a29e660735711345721537be8152c298a3342
Nyakamba <omwegaenock@gmail.com>
2023-05-24 18:37:51 +0300

    Sales.jsx

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import Title from "./utils/Title";
 import Item from "./utils/Item";
 
-const Sales = ({ endpoint }) => {
-  console.log(endpoint);
+const Sales = ({ ifExists, endpoint: { title, items } }) => {
+  //   console.log(endpoint);
   return (
     <>
-      <div>
-        <Title />
-        <div>
-          <Item />
+      <div className="nike-container">
+        <Title title={title} />
+        <div
+          className={`grid items-center justify-items-center  gap-7 lg:gap-5 mt-7 ${
+            ifExists
+              ? "grid-cols-3 xl:grid-cols-2 md:grid-cols-1"
+              : "grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1"
+          }`}
+        >
+          {items?.map((item, i) => (
+            <Item {...item} key={i} ifExists={ifExists} />
+          ))}
         </div>
       </div>
     </>
