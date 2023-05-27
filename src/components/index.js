880252d5376c46b750d858d734d1675967e429b8
Nyakamba <omwegaenock@gmail.com>
2023-05-27 23:44:17 +0300

    index.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,10 @@
 import Footer from "./Footer.jsx";
+import Navbar from "./Navbar.jsx";
 
 export { default as Hero } from "./Hero.jsx";
 export { default as Sales } from "./Sales.jsx";
 export { default as FlexContent } from "./FlexContent.jsx";
 export { default as Stories } from "./Stories.jsx";
 export { default as Footer } from "./Footer.jsx";
+export { default as Navbar } from "./Navbar.jsx";
+Navbar;
