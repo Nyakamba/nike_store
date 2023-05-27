962c9ecb309d19970a50ff012dc39f7a489e1a18
Nyakamba <omwegaenock@gmail.com>
2023-05-27 21:40:29 +0300

    index.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
+import Footer from "./Footer.jsx";
+
 export { default as Hero } from "./Hero.jsx";
 export { default as Sales } from "./Sales.jsx";
 export { default as FlexContent } from "./FlexContent.jsx";
 export { default as Stories } from "./Stories.jsx";
+export { default as Footer } from "./Footer.jsx";
