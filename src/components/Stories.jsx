95800d4eaca859edc8947efb2c25e8b96e39dc7a
Nyakamba <omwegaenock@gmail.com>
2023-05-27 21:17:19 +0300

    Stories.jsx

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -25,23 +25,36 @@ const Stories = ({ story: { title, news } }) => {
                   <div className="flex items-center justify-between w-full px-4  ">
                     <div className="flex items-center gap-0.5">
                       <HeartIcon className="icon-style text-red-500 w-5 h-5 " />
-                      <span>{val.like}</span>
+                      <span className="text-sm font-bold">{val.like}</span>
                     </div>
                     <div className="flex items-center gap-0.5">
-                      <ClockIcon className="icon-style" />
-                      <span>{val.time}</span>
+                      <ClockIcon className="icon-style w-4 h-4 text-black  " />
+                      <span className="text-sm font-bold">{val.time}</span>
                     </div>
                     <div className="flex items-center gap-0.5">
-                      <HashtagIcon className="icon-style" />
-                      <span>{val.by}</span>
+                      <HashtagIcon className="icon-style text-blue-600 " />
+                      <span className="text-sm font-bold text-blue-600 ">
+                        {val.by}
+                      </span>
                     </div>
                   </div>
-                  <div className="">
-                    <h1>{val.title}</h1>
-                    <p>{val.text}</p>
+                  <div className="grid items-center justify-items-start px-4">
+                    <h1 className="text-base font-semibold lg:text-sm">
+                      {val.title}
+                    </h1>
+                    <p className="text-sm text-justify lg:text-md">
+                      {val.text}
+                    </p>
                   </div>
-                  <div className="">
-                    <a href={val.url}>{val.btn}</a>
+                  <div className="flex items-center justify-center px-4 w-full  ">
+                    <a
+                      href={val.url}
+                      target="_blank"
+                      role={"button"}
+                      className="w-full bg-gradient-to-b from-slate-900 to-black shadow-md shadow-black text-center text-slate-100 py-1.5 button-theme "
+                    >
+                      {val.btn}
+                    </a>
                   </div>
                 </div>
               </SplideSlide>
