b73be25f3c00381dc665a83cb058b93c48e018d8
Nyakamba <omwegaenock@gmail.com>
2023-05-27 20:15:37 +0300

    Title.jsx

--- src/components/utils/Title.jsx
+++ src/components/utils/Title.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Title = ({ title }) => {
   return (
-    <div className="grid items-center">
+    <div className="grid  items-center">
       <h1 className="text-5xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter drop-shadow-lg">
-        {title}{" "}
+        {title}
       </h1>
     </div>
   );
