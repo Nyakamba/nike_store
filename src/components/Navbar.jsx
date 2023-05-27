3c6b6aebb43d966d738b0e61dc23170d3e4e1eed
Nyakamba <omwegaenock@gmail.com>
2023-05-27 23:50:49 +0300

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <header>
+        <nav>
+          <div>
+            <img src={logo} alt="logo" className="" />
+          </div>
+          <ul>
+            <li>
+              <MagnifyingGlassIcon className="icon-style" />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Navbar;
