c34cfa44f039ce3a3bd9ce241f74b20f332708dc
Nyakamba <omwegaenock@gmail.com>
2023-05-27 23:08:30 +0300

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer = ({ footerAPI: { titles, links } }) => {
+  return (
+    <>
+      <footer className="bg-theme pt-7 pb-5">
+        <div className="nike-container text-slate-200">
+          <div className="grid items-start grid-cols-3 max-w-2xl  w-full m-auto md:max-w-none md:gap-5">
+            {titles.map((val, i) => (
+              <div key={i} className="">
+                <h1>{val.title} </h1>
+              </div>
+            ))}
+            {links.map((list, i) => (
+              <ul key={i}>
+                {list.map((link, i) => (
+                  <li key={i}>{link.link} </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
