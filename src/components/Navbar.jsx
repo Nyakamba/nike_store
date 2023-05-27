c870dd50b3c64dee23e5a4f5d2984d96130f5aaf
Nyakamba <omwegaenock@gmail.com>
2023-05-28 00:21:08 +0300

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,16 +15,21 @@ const Navbar = () => {
             <img src={logo} alt="logo/img" className="w-16 h-auto" />
           </div>
           <ul className="flex items-center justify-center gap-2">
-            <li>
+            <li className="grid items-center">
               <MagnifyingGlassIcon className="icon-style" />
             </li>
-            <li>
+            <li className="grid items-center">
               <HeartIcon className="icon-style" />
             </li>
-            <li>
-              <button type="button" className="">
+            <li className="grid items-center">
+              <button
+                type="button"
+                className="border-none outline-none active:scale-110 transition-all duration-300 relative "
+              >
                 <ShoppingBagIcon className="icon-style" />
-                <div>0</div>{" "}
+                <div className="absolute top-4 right-0 bg-white text-slate-900 shadow shadow-slate-100 w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300">
+                  0
+                </div>{" "}
               </button>
             </li>
           </ul>
