3e9ff53e86e5165e927f81e6ec7485a1c958fb76
Nyakamba <omwegaenock@gmail.com>
2023-05-28 15:12:58 +0300

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,11 @@ const Navbar = () => {
       >
         <nav className="flex items-center justify-between nike-container  ">
           <div className="flex items-center">
-            <img src={logo} alt="logo/img" className="w-16 h-auto" />
+            <img
+              src={logo}
+              alt="logo/img"
+              className={`w-16 h-auto ${navState && "filter brightness-0"}`}
+            />
           </div>
           <ul className="flex items-center justify-center gap-2">
             <li className="grid items-center">
