236bfd96a97df412ed3b210452a6a1a9ddcf3cfe
Nyakamba <omwegaenock@gmail.com>
2023-05-28 16:51:50 +0300

    CartCount.jsx

--- src/components/cart/CartCount.jsx
+++ src/components/cart/CartCount.jsx
@@ -12,7 +12,7 @@ const CartCount = () => {
           <div className="grid items-center">
             <h1 className="text-base font-medium text-slate-900">
               Your Cart{" "}
-              <span className="bg-theme-cart rounded px-1 py-0.5 text-slate-100 font-normal ">
+              <span className="bg-theme-cart rounded px-1 py-0.5 text-slate-100 font-normal text-xs">
                 (Items){" "}
               </span>
             </h1>
