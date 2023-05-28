06410c4f089fb63d97dcfff04849024193ede6ae
Nyakamba <omwegaenock@gmail.com>
2023-05-28 21:06:16 +0300

    Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import CartCount from "./cart/CartCount";
 import CartEmpty from "./cart/CartEmpty";
 import CartItem from "./cart/CartItem";
-import { useDispatch } from "react-redux";
-import { setCloseCart } from "../app/CartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setCloseCart, selectCartState } from "../app/CartSlice.js";
 
 const Cart = () => {
   const dispatch = useDispatch();
+  const ifCartState = useSelector(selectCartState);
 
   const onCartToggle = () => {
     dispatch(setCloseCart({ cartState: false }));
