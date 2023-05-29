a3716ea153e29f773655fa424704566799294eb6
Nyakamba <omwegaenock@gmail.com>
2023-05-29 23:37:26 +0300

    CartItem.jsx

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -79,6 +79,7 @@ const CartItem = ({
               <button
                 type="button"
                 className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"
+                onClick={onDecreaseItemQTy}
               >
                 <MinusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white storke-[20]" />
               </button>
@@ -88,6 +89,7 @@ const CartItem = ({
               <button
                 type="button"
                 className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"
+                onClick={onIncreaseItemQTy}
               >
                 <PlusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white storke-[20]" />
               </button>
